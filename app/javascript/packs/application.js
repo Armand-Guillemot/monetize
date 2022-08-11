3252df391c2c5df8a9fa0d72c69eb0390e2dd43e
Armand Guillemot <armand.guillemot.56@gmail.com>
2022-08-11 16:24:08 +0200

    changes

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -41,4 +41,17 @@ $('.owl-carousel').owlCarousel({
  })
 });
 
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
+document.addEventListener('DOMContentLoaded', function() {
+  AOS.init({
+    disable: false,
+    startEvent: 'turbolinks:load',
+    delay: 0, // values from 0 to 3000, with step 50ms
+    duration: 400, // values from 0 to 3000, with step 50ms
+    easing: 'ease', // default easing for AOS animations
+    once: false, // whether animation should happen only once - while scrolling down
+    mirror: false, // whether elements should animate out while scrolling past them // if you are using turbolinks
+  });
+});
